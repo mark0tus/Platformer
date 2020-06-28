fcc0e37b882ad04068893606f35d9e4cb23897c1
Marko <marko.halin@tuni.fi>
2020-06-28 13:13:04 +0300

    check.point

---
+++ Assets/PlayerPos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckpointPos;
+    }
+}
