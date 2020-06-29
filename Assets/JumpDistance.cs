4d5ce1f5444f148603a3538f73a3325e5327764f
Marko <marko.halin@tuni.fi>
2020-06-29 21:35:04 +0300

    Better camera, jump gizmo

---
+++ Assets/JumpDistance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDistance : MonoBehaviour
+{
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 6);
+    }
+}
