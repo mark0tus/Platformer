afdb5cfca9b98f52a46100dac4a3c9454e8fde14
mark0tus <marko.halin@tuni.fi>
2020-07-09 16:56:14 +0300

    spikes & door

---
+++ Assets/Prefabs/Objects/WinZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WinZone : MonoBehaviour
+{
+	int playerLayer;    //The layer the player game object is on
+
+
+	void Start()
+	{
+		//Get the integer representation of the "Player" layer
+		playerLayer = LayerMask.NameToLayer("Player");
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		//If the collision wasn't with the player, exit
+		if (collision.gameObject.layer != playerLayer)
+			return;
+
+		
+		Debug.Log("Player Won!");
+		//GameManager.PlayerWon();
+	}
+}
