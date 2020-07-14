51e0d3ad2db847c05846de858bc1a04a5dd901ce
mark0tus <marko.halin@tuni.fi>
2020-07-14 10:23:27 +0300

    menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        Debug.Log("OptionsPanel");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Shutdown");
+        Application.Quit();
+    }
+}
