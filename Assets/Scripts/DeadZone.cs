b7f1d8c24c2c6d380105d08e8451730ecb4dbbf0
Marko <marko.halin@tuni.fi>
2020-07-16 23:47:18 +0300

    1

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class DeadZone : MonoBehaviour
 {
     [SerializeField] Transform spawnPoint;
+    public GameMaster gm;
+
+    private void Update()
+    {
+      //  spawnPoint = gm.lastCheckpointPos;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
             collision.transform.position = spawnPoint.position;
+
     }
 }
